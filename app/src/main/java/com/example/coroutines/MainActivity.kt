0fb3c5786f6766c0070868dad15339dea27d50e5
정영석 <0423jys@naver.com>
2024-03-11 05:08:37 +0900

    Coroutine and Thread Comparison

--- app/src/main/java/com/example/coroutines/MainActivity.kt
+++ app/src/main/java/com/example/coroutines/MainActivity.kt
@@ -8,11 +8,16 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.sync.Mutex
+import kotlinx.coroutines.sync.withLock
 
 class MainActivity : AppCompatActivity() {
 
     private var threadCount = 0
+    private val lock = Any()
+
     private var coroutineCount = 0
+    private val mutex = Mutex()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,38 +27,46 @@ class MainActivity : AppCompatActivity() {
         val coroutineBtn = findViewById<Button>(R.id.coroutineBtn)
 
         val coroutine = CoroutineScope(Dispatchers.Default)
-        val randomTimeMillis = (1000..5000).random().toLong()
 
         // 스레드 실행
         threadBtn.setOnClickListener {
-            startThread(randomTimeMillis)
+            startThread()
         }
 
 
         // 코루틴 실행
         coroutineBtn.setOnClickListener {
-            coroutine.launch(Dispatchers.Default) {
-                startCoroutine(randomTimeMillis)
+            coroutine.launch{
+                startCoroutine()
             }
         }
 
     }
 
 
-    private fun startThread(randomTimeMillis: Long) {
+    private fun startThread() {
+        val randomTimeMillis = (5000..10000).random().toLong()
         Thread {
             Thread.sleep(randomTimeMillis)
-            threadCount += 1
-            Log.d("TAG", "${threadCount}번 스레드(${randomTimeMillis}초)")
+            // 하나의 스레드에서만 값이 증가 가능
+            synchronized(lock) {
+                threadCount += 1
+                Log.d("TAG", "${threadCount}번 스레드(${randomTimeMillis}초)")
+            }
+
         }.start()
     }
 
 
-    private suspend fun startCoroutine(randomTimeMillis: Long)
+    private suspend fun startCoroutine()
     {
+        val randomTimeMillis = (1000..5000).random().toLong()
         delay(randomTimeMillis)
-        coroutineCount += 1
-        Log.d("TAG", "${coroutineCount}번 코루틴(${randomTimeMillis}초)")
+        // 하나의 코루틴에서만 값이 증가 가능
+        mutex.withLock {
+            coroutineCount += 1
+            Log.d("TAG", "${coroutineCount}번 코루틴(${randomTimeMillis}초)")
+        }
     }
 
 }
